8b7208e657679a8e13f787f19bed88078dad4378
Timm Murray <tmurray@wumpus-cave.net>
2020-01-06 16:35:48 -0600

    Update dependencies. Read wiegand input from a separate program.

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
 import * as Doorbot from '@frezik/doorbot-ts';
 import * as RPi from '@frezik/rpi-doorbot-ts';
 import * as Bodgery from '@bodgery/bodgery-doorbot-ts';
+import * as Process from 'child_process';
 
 
 const PIN = 22;
 const OPEN_TIME_MS = 30000;
 const BODGERY_API_HOST = "app.tyrion.thebodgery.org";
 const BODGERY_API_PORT = 443;
+const WIEGAND_PROGRAM = '/home/pi/wiegand_pigpio/wiegand_c';
+const WIEGAND0_PIN = 17;
+const WIEGAND1_PIN = 18;
 
 Doorbot.init_logger( "/home/pi/doorbot/doorbot.log"  )
 
 
-const reader = new RPi.WiegandReader( 17, 18 );
+// Launch wiegand reader program as its own process. Run its STDOUT into 
+// an fs reader.
+
+const wiegand = Process.spawn( WIEGAND_PROGRAM, [
+    WIEGAND0_PIN.toString()
+    ,WIEGAND1_PIN.toString()
+]);
+const reader = new Doorbot.FHReader( wiegand.stdout );
 const auth = new Bodgery.BodgeryOldAPIAuthenticator(
     BODGERY_API_HOST
     ,BODGERY_API_PORT
